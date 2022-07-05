1d1d8ac7a5c4d628bdc06030877e9244b1310575
Victor Chirino <vicchirino@gmail.com>
2022-07-05 13:11:07 -0300

    Update favicon

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -5,6 +5,7 @@ import {
 	extendTheme,
 	type ThemeConfig,
 } from "@chakra-ui/react";
+import Head from "next/head";
 
 const breakpoints = {
 	xxxsm: "100px",
@@ -68,9 +69,40 @@ const theme: ThemeConfig = extendTheme({
 
 function MyApp({ Component, pageProps }: AppProps) {
 	return (
-		<ChakraProvider theme={theme}>
-			<Component {...pageProps} />
-		</ChakraProvider>
+		<>
+			<Head>
+				<title>Victor Chirino</title>
+				<meta name="description" content="Victor Chirino personal page" />
+				<link
+					rel="apple-touch-icon"
+					sizes="180x180"
+					href="/favicon/apple-touch-icon.png"
+				/>
+				<link
+					rel="icon"
+					type="image/png"
+					sizes="32x32"
+					href="/favicon/favicon-32x32.png"
+				/>
+				<link
+					rel="icon"
+					type="image/png"
+					sizes="16x16"
+					href="/favicon/favicon-16x16.png"
+				/>
+				<link rel="manifest" href="/favicon/site.webmanifest" />
+				<link
+					rel="mask-icon"
+					href="/favicon/safari-pinned-tab.svg"
+					color="#5bbad5"
+				/>
+				<meta name="msapplication-TileColor" content="#da532c" />
+				<meta name="theme-color" content="#ffffff"></meta>
+			</Head>
+			<ChakraProvider theme={theme}>
+				<Component {...pageProps} />
+			</ChakraProvider>
+		</>
 	);
 }
 
